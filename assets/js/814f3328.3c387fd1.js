fea307963ab31dff22a9b7d250ff9a2526891825
earlution <earlution@users.noreply.github.com>
2026-06-16 11:03:43 +0000

    deploy: eb45ac1ac387ba2382aa015f34884eb1a9d903ff

--- assets/js/814f3328.3c387fd1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(self["webpackChunkportal"] = self["webpackChunkportal"] || []).push([["1833"], {
-5513(module) {
-module.exports = JSON.parse('{"title":"Recent posts","items":[{"title":"Significant release notes policy (RC ≥ 1)","permalink":"/ai-dev-kit/blog/RELEASE-NOTES-POLICY","unlisted":false,"date":"2026-06-16T09:54:57.000Z"},{"title":"Getting Docusaurus Working","permalink":"/ai-dev-kit/blog/getting-docusaurus-working","unlisted":false,"date":"2026-06-05T00:00:00.000Z"},{"title":"Welcome","permalink":"/ai-dev-kit/blog/welcome","unlisted":false,"date":"2021-08-26T00:00:00.000Z"},{"title":"MDX Blog Post","permalink":"/ai-dev-kit/blog/mdx-blog-post","unlisted":false,"date":"2021-08-01T00:00:00.000Z"},{"title":"Long Blog Post","permalink":"/ai-dev-kit/blog/long-blog-post","unlisted":false,"date":"2019-05-29T00:00:00.000Z"}]}')
-
-},
-
-}]);
